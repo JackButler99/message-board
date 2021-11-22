a8fbe8d51765c9538e66ac7214a4b2262a8f3b65
JackButler99 <ridhokurniawan32@gmail.com>
2021-11-23 01:34:39 +0700

    Add necessary code

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+const moment = require('moment')
 
+
+// Post Messages
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date().toUTCString()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date().toUTCString()
+  }
+];
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index',  { title: 'Message Board', messages: messages });
+});
+
+router.get('/new', (req, res, next) => {
+  res.render('form');
+})
+
+router.post('/new', (req, res, next) => {
+  const newMessage = {
+    text: req.body.text,
+    user: req.body.user,
+    added: new Date().toUTCString()
+  };
+  messages.unshift(newMessage);
+  res.redirect('/');
 });
 
 module.exports = router;
